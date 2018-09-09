ea3fa743a857086c5ecba851a8b6678e7ecb8711
Hiteshchandra <hiteshchandra.patel@aptushealth.com>
2018-09-09 12:18:32 +0530

    Added Inline delete, edit buttons and new order in supplier form

--- Serene-master/Serene/Serene.Core/Modules/Masters/Suppliers/SuppliersGrid.ts
+++ Serene-master/Serene/Serene.Core/Modules/Masters/Suppliers/SuppliersGrid.ts
@@ -13,4 +13,97 @@ namespace Serene.Masters {
             super(container);
         }
     }
+
+
+    import fld = Masters.PurchaseInvoiceRow.Fields;
+
+     export class CustomLinksInSuppliersGrid extends Masters.SuppliersGrid {
+
+        constructor(container: JQuery) {
+            super(container);
+        }
+
+        /**
+         * We override getColumns() to change format functions for some columns.
+         * You could also write them as formatter classes, and use them at server side
+         */
+
+          
+        protected getColumns(): Slick.Column[] {
+            var columns = super.getColumns();
+
+            columns.unshift({
+                field: 'Delete Row',
+                name: '',
+                format: ctx => '<a class="inline-action delete-row" title="delete">' +
+                    '<i class="fa fa-trash-o text-red"></i></a>',
+                width: 24,
+                minWidth: 24,
+                maxWidth: 24
+            });
+
+            columns.splice(1, 0, {
+                field: 'View Details',
+                name: '',
+                format: ctx => '<a class="inline-action view-details" title="view details"></a>',
+                width: 24,
+                minWidth: 24,
+                maxWidth: 24
+            });
+
+            columns.splice(2, 0, {
+                field: 'New Order',
+                name: '',
+                format: ctx => '<a class="inline-action new-order" title="new order"></a>',
+                width: 24,
+                minWidth: 24,
+                maxWidth: 24
+            });
+
+          return columns;
+        }
+
+        protected onClick(e: JQueryEventObject, row: number, cell: number): void {
+
+            // let base grid handle clicks for its edit links
+            super.onClick(e, row, cell);
+
+            // if base grid already handled, we shouldn"t handle it again
+            if (e.isDefaultPrevented()) {
+                return;
+            }
+
+            // get reference to current item
+            var item = this.itemAt(row);
+            var target = $(e.target);
+
+            // if user clicks "i" element, e.g. icon
+            if (target.parent().hasClass('inline-action'))
+                target = target.parent();
+
+            if (target.hasClass('inline-action')) {
+                e.preventDefault();
+
+                if (target.hasClass('delete-row')) {
+                    Q.confirm('Delete record?', () => {
+                        Masters.SuppliersService.Delete({
+                            EntityId: item.SuppliersId,
+                        }, response => {
+                            this.refresh();
+                        });
+                    });
+                }
+                else if (target.hasClass('view-details')) {
+                    this.editItem(item.SuppliersId);
+                }
+                else if (target.hasClass('new-order')) {
+                    e.preventDefault();
+                    new Masters.PurchaseInvoiceDialog().loadEntityAndOpenDialog(<Masters.PurchaseInvoiceRow>{
+                        SuppliersId: item.SuppliersId
+                    });
+                }
+            }
+        }
+
+    }
 }
